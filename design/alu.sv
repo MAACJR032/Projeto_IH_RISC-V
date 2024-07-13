69a1057277f17b3c4c28179d933826c79954673c
jambis-prg <joaovictorlima20015@gmail.com>
2024-07-13 12:22:00 -0300

    Implementedd Arithmetic Operations
    Todas as operações, menos a de subtração foram implementadas seguindo o readme principal do projeto

--- design/alu.sv
+++ design/alu.sv
@@ -17,10 +17,22 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
-            4'b0010:        // ADD
+            4'b0001:        // OR
+                    ALUResult = SrcA | SrcB;
+            4'b0010:        // ADD/ADDI
                     ALUResult = $signed(SrcA) + $signed(SrcB);
+            4'b0100:        // SLLI
+                    ALUResult = SrcA << SrcB;
+            4'b0101:        // SRLI
+                    ALUResult = SrcA >> SrcB;
+            4'b0110:        // XOR
+                    ALUResult = SrcA ^ SrcB;
+            4'b0111:        // SRAI
+                    ALUResult = $signed(SrcA) >>> SrcB;
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+            4'b1100:        // SLTI/SLT
+                   ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 1 : 0;
             default:
                     ALUResult = 0;
             endcase
--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -10,7 +10,10 @@ module imm_Gen (
     case (inst_code[6:0])
       7'b0000011:  /*I-type load part*/
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
-
+      7'b0010011: begin /*I-type arithmetic part*/
+                if (inst_code[14:12] == 3'b101) Imm_out = { 20'b0, inst_code[24:20]};
+                else Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+      end
       7'b0100011:  /*S-type*/
       Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:25], inst_code[11:7]};
 
